ee4f0e7e62d2401d3c419fc912d3dfa8e1e5a7a4
Laurynas Antanavičius <laurynas.ant@gmail.com>
2023-11-24 10:02:34 +0200

    Initial Commit

---
+++ glimpse/glimpseApp.swift
@@ -0,0 +1,17 @@
+//
+//  glimpseApp.swift
+//  glimpse
+//
+//  Created by Laurynas Antanavicius on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct glimpseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
